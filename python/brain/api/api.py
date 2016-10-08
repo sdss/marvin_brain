57fcd576ff523ae0f2b1b435773912e6243e3f78
Brian Cherinka <havok2063@hotmail.com>
2016-10-08 09:42:12 -0400

    added in additional requests exceptions, and an input timeout

--- python/brain/api/api.py
+++ python/brain/api/api.py
@@ -15,20 +15,17 @@ configkeys = []
 class BrainInteraction(object):
     """ This class defines convenience wrappers for the Brain RESTful API """
 
-    def __init__(self, route, params=None, request_type='post'):
+    def __init__(self, route, params=None, request_type='post', timeout=(3.05, 300)):
         self.results = None
         self.route = route
         self.params = params
         self.request_type = request_type
+        self.timeout = timeout
         self.statuscodes = {200: 'Ok', 401: 'Authentication Required', 404: 'URL Not Found',
                             500: 'Internal Server Error', 405: 'Method Not Allowed',
                             400: 'Bad Request', 502: 'Bad Gateway', 504: 'Gateway Timeout'}
 
-        # TODO - look into this url routing , slash either
-
         self.url = urljoin(bconfig.sasurl, route) if self.route else None
-        # self.url = self.route if bconfig.sasurl in self.route \
-        #     else os.path.join(bconfig.sasurl, route) if self.route else None
 
         if self.url:
             self._sendRequest(request_type)
@@ -40,8 +37,23 @@ class BrainInteraction(object):
                 .format(self.route, repr(self.params), self.request_type))
 
     def _checkResponse(self, response):
-        if response.status_code == 200:
-            self.status_code = 200
+        ''' Checks the response for proper http status code '''
+
+        # check for bad status
+        try:
+            isbad = response.raise_for_status()
+        except requests.HTTPError as http:
+            self.status_code = response.status_code
+            errmsg = 'Error accessing {0}: {1}-{2}'.format(response.url, response.status_code,
+                                                           self.statuscodes[response.status_code])
+            self.results = {'http_status_code': response.status_code, 'message': errmsg}
+            raise BrainError('Requests Http Status Error: {0}'.format(http))
+        else:
+            # Not bad
+            assert isbad is None, 'Http status code should not be bad'
+            assert response.ok is True, 'Ok status should be true'
+
+            self.status_code = response.status_code
             try:
                 self.results = response.json()
             except ValueError as e:
@@ -52,29 +64,32 @@ class BrainInteraction(object):
             if 'status' in self.results and self.results['status'] == -1:
                 errorMsg = 'no error message provided' \
                     if 'error' not in self.results else self.results['error']
-                raise BrainError('Something went wrong with the interaction: {0}'.format(errorMsg))
-
-        else:
-            self.status_code = response.status_code
-            errmsg = 'Error accessing {0}: {1}-{2}'.format(response.url, response.status_code,
-                                                           self.statuscodes[response.status_code])
-            self.results = {'http_status_code': response.status_code, 'message': errmsg}
-            raise BrainError('Response Error: {0}'.format(errmsg))
+                raise BrainError('Something went wrong on the server side: {0}'.format(errorMsg))
 
     def _sendRequest(self, request_type):
-
+        ''' sends the api requests to the server '''
         assert request_type in ['get', 'post'], 'Valid request types are "get" and "post".'
 
         # Loads the local config parameters
         self._loadConfigParams()
         print('brain sending these params', self.params)
         # Send the request
-        if request_type == 'get':
-            r = requests.get(self.url, params=self.params)
-        elif request_type == 'post':
-            r = requests.post(self.url, data=self.params)
-        # Check the response if it's good
-        self._checkResponse(r)
+        try:
+            if request_type == 'get':
+                self._resp = requests.get(self.url, params=self.params, timeout=self.timeout)
+            elif request_type == 'post':
+                self._resp = requests.post(self.url, data=self.params, timeout=self.timeout)
+        except requests.Timeout as rt:
+            raise BrainError('Requests Timeout Error: {0}'.format(rt))
+        except requests.URLRequired as urlreq:
+            raise BrainError('Requests Valid URL Required: {0}'.format(urlreq))
+        except requests.ConnectionError as con:
+            raise BrainError('Requests Connection Error: {0}'.format(con))
+        except requests.RequestException as req:
+            raise BrainError('Ambiguous Requests Error: {0}'.format(req))
+        else:
+            # Check the response if it's good
+            self._checkResponse(self._resp)
 
     def getData(self, astype=None):
         data = self.results['data'] if 'data' in self.results else None
@@ -106,7 +121,3 @@ class BrainInteraction(object):
             return URLMapDict(self.results['urlmap'])
         else:
             return URLMapDict()
-
-    def getQuery(self):
-        """Print SQL query."""
-        pass
