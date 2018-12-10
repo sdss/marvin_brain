54216f2aec78b0b47d5c12d24e2b79636388d018
Brian Cherinka <havok2063@hotmail.com>
2018-12-10 13:35:05 -0500

    Bump version: 0.1.1 → 0.1.2dev

--- python/brain/__init__.py
+++ python/brain/__init__.py
@@ -16,7 +16,7 @@ from brain.core.exceptions import BrainError, BrainUserWarning
 from brain.utils.general.general import merge
 
 # Set the Brain version
-__version__ = '0.1.1'
+__version__ = '0.1.2dev'
 
 
 class BrainConfig(object):
